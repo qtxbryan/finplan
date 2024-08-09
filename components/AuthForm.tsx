c7ad8a6cd06da847580795f6977240b5bdc1cd10
qtxbryan <qtxbryan@gmail.com>
2024-08-09 23:34:58 +0800

    finished authform and sign in and sign up form UI TODO: need to setup server actions in user.actions.ts, setup types properly in index.d.ts

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,9 +1,163 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import Image from "next/image";
 
-const AuthForm = ({type} : {type: string}) => {
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+} from "@/components/ui/form";
+import CustomInput from "./CustomInput";
+import { authFormSchema } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { signIn, signUp } from "@/lib/actions/user.actions";
+
+const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const formSchema = authFormSchema(type);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  const onSubmit = async(values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    setIsLoading(true);
+
+    try {
+      // Sign up and create a chatGPT token  
+      if (type === 'sign-up') {
+        const newUser = await signUp(values);
+
+        setUser(newUser);
+      }
+
+      if (type === 'sign-in') {
+        
+        const response = await signIn({
+          email: values.email,
+          password: values.password
+        })
+
+        if (response) {
+          router.push('/')
+        }
+      }
+
+      console.log(values);
+      
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+
+    setIsLoading(false);
+  }
+
   return (
-    <div>{type}</div>
-  )
-}
+    <section className="auth-form">
+      <header className="flex flex-col gap-5 md:gap-8">
+        <Link href="/" className="cursor-pointer flex items-center gap-1">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="FinPlan Logo"
+          />
+          <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+            FinPlan
+          </h1>
+        </Link>
 
-export default AuthForm
+        <div className="flex flex-col gap-1 md:gap-3">
+          <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
+            {user ? "Link Account" : type === "sign-in" ? "Sign In" : "Sign Up"}
+            <p className="text-16 font-normal text-gray-600">
+              {user
+                ? "Link your account to get started"
+                : "Please enter your details"}
+            </p>
+          </h1>
+        </div>
+      </header>
+      {user ? (
+        <div className="flex flex-col gap-4">{/* PLAIDLINK */}</div>
+      ) : (
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              {type === "sign-up" && (
+                <>
+                  <CustomInput
+                    control={form.control}
+                    name="username"
+                    label="Username"
+                    placeholder="Enter your Username"
+                  />
+                </>
+              )}
+              <CustomInput
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Enter your Email"
+              />
+              <CustomInput
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter your Password"
+              />
+              <div className="flex flex-col gap-4">
+                <Button type="submit" className="form-btn" disabled={isLoading}>
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Sign In"
+                  ) : (
+                    "Sign Up"
+                  )}
+                </Button>
+              </div>
+            </form>
+          </Form>
+
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type == "sign-in"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="form-link"
+            >
+              {type === "sign-in" ? "Sign Up" : "Sign In"}
+            </Link>
+          </footer>
+        </>
+      )}
+    </section>
+  );
+};
+
+export default AuthForm;
+
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import qs from 'query-string'
+import { z } from 'zod'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -15,3 +16,9 @@ export function formatAmount(amount: number): string {
 
   return formatter.format(amount);
 }
+
+export const authFormSchema = (type: string) => z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+  username: type === 'sign-in' ? z.string().optional() : z.string().min(3),
+});
